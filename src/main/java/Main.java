213146bff5397b231a8ad9798a6632f168494e7e
TahaLechgar <tahamr08@gmail.com>
2022-10-04 21:27:37 +0100

    add EmailSender using kavax.mail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 import Connection.ConnectionFactory;
+import Mail.MailSender;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = ConnectionFactory.getConnection();
         Statement statement = connection.createStatement();
         System.out.println(statement);
+
+        MailSender.sendMail("tahamr08@gmail.com", "Message subject", "Message body body body");
 
     }
 }
