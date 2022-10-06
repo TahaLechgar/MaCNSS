a896452b97abddcf1aeecc49c558866e6af857ed
abderrahmane <abderrahamane7@gmail.com>
2022-10-06 15:31:35 +0100

    added the get method for the patient

--- src/main/java/DAO/PatientDao.java
+++ src/main/java/DAO/PatientDao.java
@@ -1,14 +1,38 @@
 package DAO;
 
+import Connection.ConnectionFactory;
 import Models.Patient;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Optional;
 
-public class PatientDao implements Dao<Patient>{
+public class PatientDao implements Dao<Patient> {
     @Override
     public Optional<Patient> get(long id) {
-        return Optional.empty();
+        try {
+            Connection connection = ConnectionFactory.getConnection();
+
+            String query = "select fullName, birthDate, email, cin from Patient where immatricule = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setLong(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String fullName = resultSet.getString("fullName");
+                String birthdate = resultSet.getString("birthdate");
+                String email = resultSet.getString("email");
+                String cin = resultSet.getString("cin");
+                return Optional.of(new Patient(fullName, birthdate, email, cin));
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
     }
 
     @Override
