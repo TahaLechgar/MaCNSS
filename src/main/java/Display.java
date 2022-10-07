c493917e50b6f635fd02f13619f1c169166bfca8
abderrahmane <abderrahamane7@gmail.com>
2022-10-07 11:35:30 +0100

    added the auth method

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,12 +1,8 @@
-import DAO.ConjointDao;
-import DAO.Consultation;
-import DAO.FileDao;
-import DAO.MedicamentDao;
+import DAO.*;
 import Enums.Attachments;
 import Enums.State;
-import Models.Conjoint;
-import Models.File;
-import Models.Medicament;
+import Enums.UserType;
+import Models.*;
 
 import java.sql.SQLOutput;
 import java.time.LocalDate;
@@ -15,22 +11,73 @@ import java.util.*;
 public class Display {
     private final Scanner scanner = new Scanner(System.in);
 
-    public void principalMenu(){
+    public void principalMenu() {
         System.out.println("-------- Welcome to MaCNSS --------");
         System.out.println("Login as : ");
         System.out.println("1 - Admin.");
         System.out.println("2 - Agent.");
         System.out.println("3 - Patient.");
+        System.out.println("0 - Quit.");
     }
 
-    public void agentMenu(){
+    public void auth() {
+        try {
+            while (true){
+                principalMenu();
+                int choice = scanner.nextInt();
+                while (choice < 0 || choice > 3) {
+                    principalMenu();
+                    System.out.println("Enter a valid choice!!!");
+                    choice = scanner.nextInt();
+                }
+                if(choice == 0){
+                    break;
+                }
+                scanner.nextLine();
+                if (choice == 1 || choice == 2) {
+                    String username, password;
+                    System.out.print("Enter the username -> ");
+                    username = scanner.nextLine();
+                    System.out.print("Enter the password -> ");
+                    password = scanner.nextLine();
+
+                    UserType type = choice == 1 ? UserType.Admin : UserType.Agent;
+
+                    UserDao userDao = new UserDao();
+                    User user = userDao.getUserForAuth(username, password, type);
+                    if (user != null) {
+                        if (type.toString().equalsIgnoreCase("agent")) {
+                            System.out.println("Logged in as agent");
+                        } else {
+                            System.out.println("Logged in as admin");
+                        }
+                    }
+                } else {
+                    System.out.print("Enter patient immatricule -> ");
+                    long imm = scanner.nextLong();
+                    PatientDao patientDao = new PatientDao();
+                    Optional<Patient> patient = patientDao.get(imm);
+                    if (patient.isEmpty()) {
+                        System.out.println("Patient not found!!!");
+                    } else {
+                        System.out.println("logged in as patient");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void agentMenu() {
         System.out.println("Choose from the list : ");
         System.out.println("1 - Add a file.");
         System.out.println("2 - Display files.");
         System.out.println("3 - Validate a file.");
     }
 
-    public void adminMenu(){
+    public void adminMenu() {
         System.out.println("Choose from the list : ");
         System.out.println("1 - Add an agent.");
         System.out.println("2 - Update an agent.");
@@ -38,33 +85,33 @@ public class Display {
         System.out.println("4 - Display agents.");
     }
 
-    public void patientMenu(){
+    public void patientMenu() {
         System.out.println("Choose from the list : ");
         System.out.println("1 - Display all files");
         System.out.println("1 - Display a specific file");
     }
 
-    public void displayAllFiles(Integer patientImm){
+    public void displayAllFiles(Integer patientImm) {
         FileDao fileDao = new FileDao();
         List<File> files;
         int i = 1;
-       files = (patientImm == null) ? fileDao.getAll() : fileDao.getFileOfPatient(patientImm);
-        for(File file: files){
+        files = (patientImm == null) ? fileDao.getAll() : fileDao.getFileOfPatient(patientImm);
+        for (File file : files) {
             System.out.println(" " + i + " | " + file.getConstultationType() + " \t| " + file.getDepositionDate() + " | " + file.getState());
             i++;
         }
         System.out.println("Choose a file from the list to see its details : ");
         int choice;
-        do{
+        do {
             choice = Integer.parseInt(scanner.nextLine());
-        }while (choice >= i || choice < 1);
+        } while (choice >= i || choice < 1);
 
         long chosenFileID = files.get(choice).getId();
         System.out.println(chosenFileID);
     }
 
 
-    public void addFile(){
+    public void addFile() {
         FileDao fileDao = new FileDao();
         Consultation consultation = Consultation.consultations.get(chooseConsultation() - 1);
         String consultationType = consultation.getType();
@@ -72,7 +119,7 @@ public class Display {
         String depositDate = LocalDate.now().toString();
         long patientImm = enterPatientImm();
         long conjointIdCheck = conjoint(patientImm);
-        Long conjoint_id = ( conjointIdCheck == 0) ? null : conjointIdCheck;
+        Long conjoint_id = (conjointIdCheck == 0) ? null : conjointIdCheck;
         System.out.println("conjoint id : " + conjoint_id);
         State state = State.PENDING;
         float repaymentAmount = consultation.getRefundPrice();
@@ -80,13 +127,13 @@ public class Display {
         ArrayList<Medicament> medicaments = null;
         HashMap<String, Float> attachments = null;
 
-        if(isConsultationExpired(consultationDate)){
+        if (isConsultationExpired(consultationDate)) {
             state = State.REFUSED;
             repaymentAmount = 0;
-        }else{
-            if(consultation.isRefundable()){
+        } else {
+            if (consultation.isRefundable()) {
                 medicaments = joinMedicaments();
-                for(Medicament medicament: medicaments){
+                for (Medicament medicament : medicaments) {
                     repaymentAmount += medicament.getPrixRemboursement();
                 }
                 attachments = joinAttachments();
@@ -104,106 +151,105 @@ public class Display {
         MedicamentDao medicamentDao = new MedicamentDao();
         ArrayList<Medicament> medicaments = new ArrayList<>();
         long codeBarre;
-        do{
+        do {
             System.out.println("insert the codeBarre of medicament : ");
             codeBarre = Long.parseLong(scanner.nextLine());
-            if(codeBarre == 0){
+            if (codeBarre == 0) {
                 break;
             }
             Optional<Medicament> optionalMedicament = medicamentDao.get(codeBarre);
-            if(optionalMedicament.isPresent()){
+            if (optionalMedicament.isPresent()) {
                 medicaments.add(optionalMedicament.get());
-            }else {
+            } else {
                 System.out.println("Medicament doesn't exist .. try again !");
             }
-        }while(true);
+        } while (true);
         return medicaments;
     }
 
 
-    private String chooseConsultationDate(){
+    private String chooseConsultationDate() {
         System.out.println("Choose consultation date format as YYYY-MM-DD : ");
         String string = scanner.nextLine();
         System.out.println(string);
         return string;
     }
 
-    private long enterPatientImm(){
+    private long enterPatientImm() {
         System.out.println("Enter patient Matriculate :");
         return Long.parseLong(scanner.nextLine());
     }
 
 
-    private boolean isConjoint(){
+    private boolean isConjoint() {
         System.out.println("This file is for : ");
         System.out.println(" 1 - patient.");
         System.out.println(" 2 - conjoint.");
         int choice = Integer.parseInt(scanner.nextLine());
-        while (choice != 1 && choice != 2){
+        while (choice != 1 && choice != 2) {
             choice = Integer.parseInt(scanner.nextLine());
         }
         return choice == 2;
     }
 
-    private long chooseConjoint(long patientImm){
+    private long chooseConjoint(long patientImm) {
         ArrayList<Conjoint> conjoints;
         ConjointDao conjointDao = new ConjointDao();
 
         conjoints = (ArrayList<Conjoint>) conjointDao.getConjointsForPatient(patientImm);
         System.out.println("Choose conjoint from the list : ");
         int i = 1;
-        for (Conjoint conjoint: conjoints){
-            System.out.println( " " + i + " - " + conjoint.getFullName());
+        for (Conjoint conjoint : conjoints) {
+            System.out.println(" " + i + " - " + conjoint.getFullName());
             i++;
         }
         int choice = Integer.parseInt(scanner.nextLine());
-        while (choice >=i || choice < 1){
+        while (choice >= i || choice < 1) {
             choice = Integer.parseInt(scanner.nextLine());
         }
         return conjoints.get(choice - 1).getId();
     }
 
 
-
-    private long conjoint(long patientImm){
-        if(!isConjoint()){
+    private long conjoint(long patientImm) {
+        if (!isConjoint()) {
             return 0;
         }
         return chooseConjoint(patientImm);
     }
 
-    private int chooseConsultation(){
+    private int chooseConsultation() {
         System.out.println("Choose consultation type : ");
         int i = 1;
-        for(Consultation consultaion: Consultation.consultations){
+        for (Consultation consultaion : Consultation.consultations) {
             System.out.println(" " + i + " - " + consultaion.getType());
             i++;
         }
         int choice = Integer.parseInt(scanner.nextLine());
-        while(choice >= i || choice < 1){
+        while (choice >= i || choice < 1) {
             choice = Integer.parseInt(scanner.nextLine());
         }
         return choice;
     }
 
-    private HashMap<String, Float> joinAttachments(){
+    private HashMap<String, Float> joinAttachments() {
         System.out.println("Add your attachments (enter 0 to finish) :");
 
         HashMap<String, Float> attachments = new HashMap<>();
 
-        while (true){
+        while (true) {
             int i = 1;
-            for(Attachments attachment: Attachments.values()){
+            for (Attachments attachment : Attachments.values()) {
                 System.out.println(" " + i + " - " + attachment);
                 i++;
             }
             int choice;
-            do{
+            do {
                 choice = Integer.parseInt(scanner.nextLine());
-                if(choice == 0)
+                if (choice == 0)
                     return attachments;
 
-            }while(choice >= i || choice < 1);
+            } while (choice >= i || choice < 1);
             Attachments chosenAttachment = Attachments.values()[choice - 1];
 
             System.out.println("Enter price : ");
@@ -213,7 +259,7 @@ public class Display {
         }
     }
 
-    private boolean isConsultationExpired(String consultationDate){
+    private boolean isConsultationExpired(String consultationDate) {
 
         String[] splitedDate = consultationDate.split("-", 0);
 
